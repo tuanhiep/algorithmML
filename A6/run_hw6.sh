f1d03c218d7cd22b8d9ae64ffa7f5c67a35885e2
bigname <tuanhiep.tran88@gmail.com>
2020-04-20 18:24:22 -0600

    update script to run the experiments

--- A6/run_hw6.sh
+++ A6/run_hw6.sh
@@ -4,31 +4,54 @@ echo "Starting..."
 
 # Prepare second datasets
 echo "Preparing second datasets..."
-python -W ignore prepare_dataset.py csv/all_breakdown.csv csv/all_breakdown-clean.csv
-
+python -W ignore prepare_dataset.py csv/all_breakdown.csv csv/all_breakdown_fine.csv
+echo "DOING EXPERIMENTS:"
 #HOUSING DATA SET
-dataset="../csv/housing.data.txt"
+echo "HOUSING DATA SET"
+dataset="csv/housing.data.txt"
 echo "Do experiment with $dataset data set: "
 for regressor in "lr" "lrne"; do
   python -W ignore main.py -data $dataset "-${regressor}"
 done
 
-python -W ignore main.py -data $dataset -lasso -alpha 1.0
-python -W ignore main.py -data $dataset -ridge -alpha 1.0
-python -W ignore main.py -data $dataset -ransac -min_sample 50
-python -W ignore main.py -data $dataset -nlr -max_depth 13
+for alpha in  0.1 1 10; do
+  echo "ALPHA = ${alpha}"
+  python -W ignore main.py -data $dataset -lasso -alpha ${alpha}
+  python -W ignore main.py -data $dataset -ridge -alpha ${alpha}
+done
+
+for min_sample in 50 150 250 ; do
+  echo "MIN SAMPLE = ${min_sample}"
+  python -W ignore main.py -data $dataset -ransac -min_sample ${min_sample}
+done
+
+for max_depth in  5 9 13; do
+  echo "MAX DEPTH = ${max_depth}"
+  python -W ignore main.py -data $dataset -nlr -max_depth ${max_depth}
+done
 
 #ALL BREAKDOWN DATA SET
 echo "ALL BREAKDOWN DATA SET"
-dataset="../csv/all_breakdown_fine.csv"
+dataset="csv/all_breakdown_fine.csv"
 echo "Do experiment with $dataset data set: "
 for regressor in "lr" "lrne"; do
   python -W ignore main.py -data $dataset "-${regressor}"
 done
 
-python -W ignore main.py -data $dataset -lasso -alpha 1.0
-python -W ignore main.py -data $dataset -ridge -alpha 1.0
-python -W ignore main.py -data $dataset -ransac -min_sample 50
-python -W ignore main.py -data $dataset -nlr -max_depth 13
+for alpha in  0.1 1 10; do
+  echo "ALPHA = ${alpha}"
+  python -W ignore main.py -data $dataset -lasso -alpha ${alpha}
+  python -W ignore main.py -data $dataset -ridge -alpha ${alpha}
+done
+
+for min_sample in 50 150 250 ; do
+  echo "MIN SAMPLE = ${min_sample}"
+  python -W ignore main.py -data $dataset -ransac -min_sample ${min_sample}
+done
+
+for max_depth in 3 5 7; do
+  echo "MAX DEPTH = ${max_depth}"
+  python -W ignore main.py -data $dataset -nlr -max_depth ${max_depth}
+done
 
 echo "End !"
